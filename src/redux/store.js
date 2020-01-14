f304fbcedaf0df2acc0e510d3ce6e5b2f1768b1a
claudioquirozipi <claudioquirozipi@gmail.com>
2020-01-14 15:21:37 -0500

    cambio en la configuración del store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 // const initialState = {};
 const initialState = {};
@@ -10,9 +11,13 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // compose(
+  //   applyMiddleware(...middleware),
+  //   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
+  composeWithDevTools(
+    /* logger must be the last middleware in chain to log actions */
+    applyMiddleware(thunk)
   )
 );
 
